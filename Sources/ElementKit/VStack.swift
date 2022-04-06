fbca715eed68f31aa588bbb2fa4905233ac3b739
chwdw92 <64773051+chwdw92@users.noreply.github.com>
2022-04-06 13:00:01 -0700

    Update VStack.swift

--- Sources/ElementKit/VStack.swift
+++ Sources/ElementKit/VStack.swift
@@ -5,11 +5,11 @@
 //  Created by David Won on 2022-03-12.
 //
 
-import UIKit
+// import UIKit
 
-open class VStack: BaseUIStackView {
-    open override func setupView() {
-        super.setupView()
-        axis = .vertical
-    }
-}
+// open class VStack: BaseUIStackView {
+//     open override func setupView() {
+//         super.setupView()
+//         axis = .vertical
+//     }
+// }
